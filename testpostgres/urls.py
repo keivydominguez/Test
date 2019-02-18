3159e4cd5973e20d2ab66c06eef1c4eb8ceedaa1
Keivy Sanchez <keivysanchez@gmail.com>
2019-02-18 12:09:03 -0600

    modificaciones al urly

--- testpostgres/urls.py
+++ testpostgres/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('producto/', include('producto.urls')),
+    path('', include('producto.urls')),
     path('admin/', admin.site.urls),
 ]
