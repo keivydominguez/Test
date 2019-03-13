dcb3dab536357945ab90c7b69f674daa1400d190
Keivy Sanchez <keivysanchez@gmail.com>
2019-03-13 15:13:29 -0600

    for y while

--- ejercicio2/urls.py
+++ ejercicio2/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('ejercicio2/', views.fecha, name='fecha'),
+    path('ejercicio2/abc/', views.abc, name='abd'),
+    path('ejercicio2/for_palabras/', views.for_palabras, name='for_palabras'),
+    path('ejercicio2/while_palabra/', views.while_palabra, name='while_palabra'),
+    path('ejercicio2/regresar/', views.regresar, name='regresar'),
+    path('ejercicio2/whileabc/', views.whileabc, name='whileabc'),
+    path('ejercicio2/whileregresa/', views.whileregresa, name='whileregresa'),
+
 ]
--- ejercicio2/views.py
+++ ejercicio2/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import datetime
 
@@ -61,7 +62,6 @@ def fecha(request):
             x = x + imprimirdia
 
         x = x + diadia
-
     dic = {
         "bienvenido": "Bienvenido",
         "semana": semana,
@@ -79,3 +79,218 @@ def fecha(request):
 
     return render(request, 'ejercicio2/fecha.html', dic)
 
+
+def abc(request):
+    abcarreglo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"]
+
+    if request.method == "POST":
+        abc = request.POST.get("abc")
+
+        acomodar = ""
+
+        for pal in abc:
+            if pal == " ":
+                acomodar = acomodar + " "
+            else:
+                x = 0
+                for foo in abcarreglo:
+                    if x == 24:
+                        if foo == pal:
+                            acomodar = acomodar + abcarreglo[0]
+                    else:
+                        if foo == pal:
+                            acomodar = acomodar + abcarreglo[x + 1]
+                    x = x + 1
+
+    dic = {
+        "text": "Bienvenido",
+        "acomodar": acomodar,
+    }
+
+    return render(request, 'ejercicio2/abc.html', dic)
+
+
+def regresar(request):
+
+
+    abcarreglo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"]
+
+    if request.method == "POST":
+        regresar = request.POST.get("regresar")
+
+        acomodar1 = ""
+
+        for pal in regresar:
+            if pal == " ":
+                acomodar1 = acomodar1 + " "
+            else:
+                x = 0
+                for foo in abcarreglo:
+                    if x == 24:
+                        if foo == pal:
+                            acomodar1 = acomodar1 + abcarreglo[0]
+                    else:
+                        if foo == pal:
+                            acomodar1 = acomodar1 + abcarreglo[x - 1]
+                    x = x + 1
+
+
+    dic = {
+        "text": "Bienvenido",
+        "acomodar1": acomodar1,
+    }
+
+    return render(request, 'ejercicio2/abc.html', dic)
+
+
+def whileabc(request):
+    abcarreglo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"]
+
+    if request.method == "POST":
+        abc = request.POST.get("abc")
+
+        acomodar = ""
+
+        pal = 0
+        while pal < len(abc):
+            if abc[pal] == " ":
+                acomodar = acomodar + " "
+            else:
+                x = 0
+                foo = 0
+                while foo < len(abcarreglo):
+                    if x == 24:
+                        if abcarreglo[foo] == abc[pal]:
+                            acomodar = acomodar + abcarreglo[0]
+                    else:
+                        if abcarreglo[foo] == abc[pal]:
+                            acomodar = acomodar + abcarreglo[x + 1]
+                    x = x + 1
+
+                    foo = foo + 1
+
+            pal = pal + 1
+
+    dic = {
+        "text": "Bienvenido",
+        "acomodar": acomodar,
+
+    }
+    return render(request, 'ejercicio2/whileabc.html', dic)
+
+def whileregresa(request):
+    abcarreglo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V", "W", "X", "Y", "Z"]
+
+    if request.method == "POST":
+        regresar = request.POST.get("regresar")
+
+        acomodar1 = ""
+
+        pal = 0
+        while pal < len(regresar):
+            if regresar[pal] == " ":
+                acomodar1 = acomodar1 + " "
+            else:
+                x = 0
+                foo = 0
+                while foo < len(abcarreglo):
+                    if x == 24:
+                        if abcarreglo[foo] == regresar[pal]:
+                            acomodar1 = acomodar1 + abcarreglo[0]
+                    else:
+                        if abcarreglo[foo] == regresar[pal]:
+                            acomodar1 = acomodar1 + abcarreglo[x - 1]
+                    x = x + 1
+
+                    foo = foo + 1
+
+            pal = pal + 1
+
+    dic = {
+        "text": "Bienvenido",
+        "acomodar1": acomodar1,
+    }
+
+    return render(request, 'ejercicio2/whileabc.html', dic)
+
+
+
+def for_palabras(request):
+    aux = ""
+    caja = []
+
+    if request.method == "POST":
+        letra = request.POST.get("letra")
+
+    for i in letra:
+        if i not in aux:
+            aux = aux + i
+
+    x = 0
+    for a in aux :
+        cont = 0
+        for b in letra:
+            if a == b:
+                cont = cont + 1
+        caja.append(cont)
+    x = x + 1
+
+    resultado = []
+    v = 0
+    for e in aux:
+        resultado.append({
+            'aux': aux[v],
+            'caja': caja[v],
+        })
+        v = v + 1
+
+    dic = {
+        "text": "Bienvenido",
+        "aux": aux,
+        "caja": caja,
+        "resultado": resultado,
+    }
+
+    return render(request, 'ejercicio2/for_palabras.html', dic)
+
+
+def while_palabra(request):
+    aux = ""
+    caja = []
+
+    if request.method == "POST":
+        letra = request.POST.get("letra")
+
+    i = 0
+    while i < len(letra):
+        if letra[i] not in aux:
+            aux = aux + letra[i]
+        i = i +1
+
+    a = 0
+    while a < len(aux):
+        cont = 0
+        b=0
+        while b < len(letra):
+            if aux[a] == letra[b]:
+                cont = cont + 1
+            b = b + 1
+        caja.append(cont)
+        a = a + 1
+
+    resultado = []
+    v = 0
+    while v < len(aux):
+        resultado.append({
+            'aux': aux[v],
+            'caja': caja[v]
+        })
+        v = v + 1
+
+    dic = {
+        "text": "Bienvenido",
+        "aux": aux,
+        "caja": caja,
+        "resultado": resultado,
+    }
+    return render(request, 'ejercicio2/for_palabras.html', dic )
